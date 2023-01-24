ae8471df3133671aeee9b9de99fe22d35b146355
newProgrammerLR <antoniopablontroncoso@gmail.com>
2023-01-24 08:24:29 -0300

    Partials Casi Listos

--- routers/main.js
+++
@@ -1,7 +0,0 @@
-const express = require ("express")
-const controlador = require("../controllers/mainControllers.js")
-
-const router = express.Router()
-router.get ("/", controlador.index)
-
-module.exports = router
